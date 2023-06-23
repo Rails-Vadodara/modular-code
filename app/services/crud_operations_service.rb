99e2430382753c53284448a1ff851517d21335da
JigarShahTntra <jigar.shah@tntra.io>
2023-06-23 17:12:33 +0530

    Accessible from console.

--- app/services/crud_operations_service.rb
+++ app/services/crud_operations_service.rb
@@ -7,7 +7,7 @@ class CrudOperationsService
 
   def initialize(model, params = {})
     @model = model
-    @params = params
+    @params = valid_params? ? params : ActionController::Parameters.new(params)
   end
 
   def index
