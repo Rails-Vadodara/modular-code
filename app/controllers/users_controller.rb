33edd452d55206b9314a03c3f06a23ea93398e9f
JigarShahTntra <jigar.shah@tntra.io>
2023-06-28 12:17:28 +0530

    initial commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,7 @@ class UsersController < ApplicationController
   %w[show edit create update destroy index new].each do |no_params_method|
     define_method no_params_method do
       instance_variable_set("@#{variable_name}", CrudOperationsService.new(User, params).send(no_params_method))
-      respond_to do |format|
-        format.html { redirect_to redirection if redirection }
-        format.json { render :show, status: :created, location: @project }
-      end
+      redirection
     end
   end
 
--- app/services/concerns/custom_router.rb
+++ app/services/concerns/custom_router.rb
@@ -1,11 +1,45 @@
 module CustomRouter
-  REDIRECTION_CONFIG = YAML.load(File.open('config/action_redirection.yml'))
+  REDIRECTION_CONFIG = YAML.load(File.open('config/action_redirection.yml'))['action_redirection']
 
   def redirection
-    controller_routes = REDIRECTION_CONFIG['action_redirection'][controller_name]
-    return false unless controller_routes[action_name].present?
+    respond_to do |format|
+      format.html { redirect_to url if singular?(variable_name) }
+      format.json { render :show, status: :created, location: @project }
+    end
+  end
 
-    url_for(controller: controller_name, action: controller_routes[action_name], id: instance_variable_get("@#{variable_name}")&.id,
+  private
+
+  def redirections
+    redirect_to url if action_route.present?
+  end
+
+  def instance_variable
+    instance_variable_get("@#{variable_name}")
+  end
+
+  def fallback_check
+    singular?(variable_name) && instance_variable.errors.present? ? errored_route : action_route
+  end
+
+  def errored_route
+    REDIRECTION_CONFIG[controller_name]['fallbacks'][action_name]
+  end
+
+  def action_route
+    REDIRECTION_CONFIG[controller_name][action_name]
+  end
+
+  def url
+    url_for(controller: controller_name, action: fallback_check, id: variable_id,
             only_path: true)
+  end
+
+  def singular?(variable_name)
+    variable_name.pluralize != variable_name
+  end
+
+  def variable_id
+    instance_variable&.id
   end
 end
