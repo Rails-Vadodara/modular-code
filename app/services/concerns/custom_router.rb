95e43b62534b9bb57a860b7222a95c787e118b57
JigarShahTntra <jigar.shah@tntra.io>
2023-07-03 15:03:25 +0530

    Changed MVC handling for all the pages with custom router. HTML and Turbo Stream Handling done.

--- app/services/concerns/custom_router.rb
+++ app/services/concerns/custom_router.rb
@@ -3,35 +3,36 @@ module CustomRouter
 
   def redirection
     respond_to do |format|
-      format.html { redirect_to url if singular?(variable_name) }
+      format.turbo_stream do
+        eval("#{render_or_redirect}") if REDIRECTION_CONFIG[controller_name]['turbo_stream'].keys.include?(action_name)
+      end
+      format.html do
+        eval("#{render_or_redirect}") if REDIRECTION_CONFIG[controller_name]['html'].keys.include?(action_name)
+      end
       format.json { render :show, status: :created, location: @project }
     end
   end
 
   private
 
-  def redirections
-    redirect_to url if action_route.present?
+  def render_or_redirect
+    singular?(variable_name) && instance_variable.errors.present? ? "render '#{errored_route}'" : "redirect_to '#{url}'"
   end
 
   def instance_variable
     instance_variable_get("@#{variable_name}")
   end
 
-  def fallback_check
-    singular?(variable_name) && instance_variable.errors.present? ? errored_route : action_route
-  end
-
   def errored_route
-    REDIRECTION_CONFIG[controller_name]['fallbacks'][action_name]
+    REDIRECTION_CONFIG.dig(controller_name, request.format.to_sym.to_s, 'fallbacks', action_name)
   end
 
   def action_route
-    REDIRECTION_CONFIG[controller_name][action_name]
+    REDIRECTION_CONFIG.dig(controller_name, request.format.to_sym.to_s, action_name)
   end
 
   def url
-    url_for(controller: controller_name, action: fallback_check, id: variable_id,
+    url_for(controller: controller_name, action: action_route, id: variable_id,
             only_path: true)
   end
 
